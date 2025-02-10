46f89383b27960f436fb155f142b8bcbc062c2d2
Raychaser <198485567+RaychaserRegolith@users.noreply.github.com>
2025-02-10 14:41:01 -0800

    fixing the multiplier problem

--- src/loadoutCalc.ts
+++ src/loadoutCalc.ts
@@ -12,9 +12,6 @@ import { roundFloat } from './util'
 export const DEFAULT_MOLE_LASER = MiningLaserEnum.ArborMh2
 export const DEFAULT_PROSPECTOR_LASER = MiningLaserEnum.ArborMh1
 
-export function multiplyReduceModules(modules: MiningModule[], stat: keyof AllStats): number {
-  return modules.reduce((acc, m) => acc * (m.stats[stat] || 1), 1)
-}
 export function multiplyReduceLasers(lasers: LaserLoadoutStats[], stat: keyof AllStats): number {
   return lasers.reduce((acc, l) => acc * (l[stat] || 1), 1)
 }
@@ -45,7 +42,9 @@ export async function calcLoadoutStats(ds: DataStore, miningLoadout: MiningLoado
 
   const laserStats: AllStats[] = await Promise.all(laserStatsPromises)
   const activeGadget =
-    miningLoadout.activeGadgetIndex > -1 && miningLoadout.inventoryGadgets.length > miningLoadout.activeGadgetIndex
+    miningLoadout.activeGadgetIndex &&
+    miningLoadout.activeGadgetIndex > -1 &&
+    miningLoadout.inventoryGadgets.length > miningLoadout.activeGadgetIndex
       ? cloneDeep(loadoutLookup.gadgets[miningLoadout.inventoryGadgets[miningLoadout.activeGadgetIndex]])
       : null
   const gadgetStats = sanitizeStats(activeGadget?.stats || {})
@@ -55,11 +54,13 @@ export async function calcLoadoutStats(ds: DataStore, miningLoadout: MiningLoado
     const laser = loadoutLookup.lasers[al.laser as MiningLaserEnum]
     return acc + (laser.stats.maxPower || 0)
   }, 0)
+
   const totalUnModdedExtrPower = activeLasers.reduce((acc, al) => {
     if (!al || !al.laser || !al.laserActive) return acc
     const laser = loadoutLookup.lasers[al.laser as MiningLaserEnum]
     return acc + (laser.stats.extrPower || 0)
   }, 0)
+
   const totalModdedPower = laserStats.reduce((acc, ls) => acc + ls.maxPower, 0)
   const totalModdedExtrPower = laserStats.reduce((acc, ls) => acc + ls.extrPower, 0)
 
@@ -93,15 +94,16 @@ export async function calcLoadoutStats(ds: DataStore, miningLoadout: MiningLoado
     // Get the minimum value or 0 if there are no lasers
     optimumRange: laserStats.length > 0 ? addReduceLasers(laserStats, 'optimumRange') / laserStats.length : 0,
 
-    overchargeRate: multiplyReduceLasers(laserStats, 'overchargeRate') * gadgetStats.overchargeRate,
-    clusterMod: multiplyReduceLasers(laserStats, 'clusterMod') * gadgetStats.clusterMod,
-    inertMaterials: multiplyReduceLasers(laserStats, 'inertMaterials') * gadgetStats.inertMaterials,
+    overchargeRate: multiplyReduceLasers(laserStats, 'overchargeRate') * (gadgetStats.overchargeRate || 1),
+    clusterMod: multiplyReduceLasers(laserStats, 'clusterMod') * (gadgetStats.clusterMod || 1),
+    inertMaterials: multiplyReduceLasers(laserStats, 'inertMaterials') * (gadgetStats.inertMaterials || 1),
 
-    instability: multiplyReduceLasers(laserStats, 'instability') * gadgetStats.instability,
-    resistance: multiplyReduceLasers(laserStats, 'resistance') * gadgetStats.resistance,
-    optimalChargeRate: multiplyReduceLasers(laserStats, 'optimalChargeRate') * gadgetStats.optimalChargeRate,
-    optimalChargeWindow: multiplyReduceLasers(laserStats, 'optimalChargeWindow') * gadgetStats.optimalChargeWindow,
-    shatterDamage: multiplyReduceLasers(laserStats, 'shatterDamage') * gadgetStats.shatterDamage,
+    instability: multiplyReduceLasers(laserStats, 'instability') * (gadgetStats.instability || 1),
+    resistance: multiplyReduceLasers(laserStats, 'resistance') * (gadgetStats.resistance || 1),
+    optimalChargeRate: multiplyReduceLasers(laserStats, 'optimalChargeRate') * (gadgetStats.optimalChargeRate || 1),
+    optimalChargeWindow:
+      multiplyReduceLasers(laserStats, 'optimalChargeWindow') * (gadgetStats.optimalChargeWindow || 1),
+    shatterDamage: multiplyReduceLasers(laserStats, 'shatterDamage') * (gadgetStats.shatterDamage || 1),
 
     extrPowerMod,
     minPowerPct,
@@ -111,6 +113,24 @@ export async function calcLoadoutStats(ds: DataStore, miningLoadout: MiningLoado
     priceNoStock: laserStats.reduce((acc, l) => (l.priceNoStock ? acc + l.priceNoStock : acc), 0) + inventoryPrice,
   }
 
+  return retVal
+}
+
+/**
+ * Feb 10, 2025: As of 4.0.1 modules stack again. What this means is that the laser power (l) is
+ * (l * m1) + (l * m2) + (l * m3) + ... + (l * mn) where m1, m2, m3, ... mn are the module multipliers
+ * @param modules
+ * @param stat
+ * @returns
+ */
+export function multiplyReduceModules(modules: MiningModule[], stat: keyof AllStats): number {
+  // THIS IS THE OLD WAY. Leaving it here for reference
+  // return modules.reduce((acc, m) => acc * (m.stats[stat] || 1), 1)
+  const retVal =
+    modules.reduce((acc, m) => {
+      if (!m) return acc
+      return acc + (m.stats[stat] || 1) - 1
+    }, 0) + 1
   return retVal
 }
 
